9342b2e1dc54e4f0eed32c27e5f3c7961446bd64
Navdeep Singh <navdeep.er@gmail.com>
2021-01-19 12:28:43 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+interface iProps {
+  step?: number
+  initialCount?: number
+}
+
+interface iState {
+  count: number
+}
+
+type Action =
+  | {type: 'increment'; count: number}
+  | {type: 'decrement'; count: number}
+
+// 1st agrument current state
+// 2nd argument dispatch function or new state
+function countReducer(state: iState, action: Action): iState {
+  switch (action.type) {
+    case 'increment':
+      return {count: state + 1}
+    case 'decrement':
+      return {count: state - 1}
+    default:
+      return state
+  }
+}
+
+const Counter: React.FC<iProps> = ({step = 1, initialCount = 0}) => {
+  const [count, dispatch] = React.useReducer(countReducer, initialCount)
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>{count}</h1>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gridGap: '0.75rem',
+        }}
+      >
+        <button onClick={() => dispatch({type: 'increment', count})}>+</button>
+        <button onClick={() => dispatch(count - step)}>-</button>
+      </div>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Counter />
+    </>
+  )
 }
 
-export default App;
+export default App
